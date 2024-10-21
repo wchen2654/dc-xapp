3d2a1e01cc6c5dfc5bcb65d1768e83c62e0e524d
Wen Chen <wchen2654@gmail.com>
2024-10-21 14:28:20 -0500

    Changes

--- src/nexran.cc
+++ src/nexran.cc
@@ -200,7 +200,7 @@ bool App::handle(e2sm::kpm::KpmIndication *kind)
 {
 
 	auto influxdb = influxdb::InfluxDBFactory::Get("http://10.97.189.80:8086?db=_internal");
-	influxdb->createDatabaseIfNotExists();
+	// influxdb->createDatabaseIfNotExists();
 
     mdclog_write(MDCLOG_INFO,"KpmIndication: %s",
 		 kind->report->to_string('\n',',').c_str());
