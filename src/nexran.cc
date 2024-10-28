8dab2e63df2c720e273404c13564cd7812ea2638
Wen Chen <wchen2654@gmail.com>
2024-10-28 10:26:36 -0500

    Changes

--- src/nexran.cc
+++ src/nexran.cc
@@ -700,7 +700,7 @@ void App::start()
 			std::cout << "InfluxDB connection succeeded, data fetched." << std::endl;
 			for (auto& point: result)
 			{
-					mdclog_write(MDCLOG_INFO, "%x", point.getFields());
+					mdclog_write(MDCLOG_INFO, "%s", point.getFields());
 			}
 		} else {
 				std::cout << "InfluxDB connection succeeded, but no data found." << std::endl;
