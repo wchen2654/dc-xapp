20e02d2b13de405708543addd449838721664c1b
Wen Chen <wchen2654@gmail.com>
2024-11-07 14:28:53 -0600

    Changes

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
-from mdclogpy import Logger
+# from mdclogpy import Logger
+import sys
 
 def add(num1, num2):
 
-    myLogger.info("Result is: " + str(num1 + num2))
+    # myLogger.info("Result is: " + str(num1 + num2))
+
+    print(sys.version)
+    print(sys.executable)
 
     return num1 + num2
 
-def main():
+# def main():
 
-    myLogger = Logger()
-    myLogger.mdclog_format_init(configmap_monitor=True)
-    myLogger.info("This is an info log")
-    myLogger.error("This is an error log")
+#     myLogger = Logger()
+#     myLogger.mdclog_format_init(configmap_monitor=True)
+#     myLogger.info("This is an info log")
+#     myLogger.error("This is an error log")
 
-main()
+# main()
