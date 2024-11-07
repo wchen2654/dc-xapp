8c69e80e8cfe75db5c9e4f6c7b893da9d10be03e
Wen Chen <wchen2654@gmail.com>
2024-11-07 13:16:53 -0600

    Changes

--- src/nexran.cc
+++ src/nexran.cc
@@ -702,7 +702,9 @@ void App::start()
         std::cerr << "Failed to open directory: " << strerror(errno) << std::endl;
     }
 
-	PySys_SetPath(cwd);
+	const wchar_t* pythonPath = L"/";
+
+	PySys_SetPath(pythonPath);
 
 	PyObject *pName = PyUnicode_DecodeFSDefault("main");  // Module name (example.py)
     PyObject *pModule = PyImport_Import(pName);
