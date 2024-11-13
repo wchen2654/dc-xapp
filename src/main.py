022848bd3154067b424443a0f1af9d9b4d398b34
Wen Chen <wchen2654@gmail.com>
2024-11-13 10:19:33 -0600

    Code changes

--- src/main.py
+++ src/main.py
@@ -10,11 +10,11 @@ def add(num1, num2):
 
     return num1 + num2
 
-def main():
+# def main():
 
     # myLogger = Logger()
     # myLogger.mdclog_format_init(configmap_monitor=True)
     # myLogger.info("This is an info log")
     # myLogger.error("This is an error log")
 
-main()
+# main()
