fee1c01f8a6299dda2c4eab6de37265997a78d0a
Wen Chen <wchen2654@gmail.com>
2024-11-25 14:58:21 -0600

    Modify Metrics

--- src/intrusionDetection.py
+++ src/intrusionDetection.py
@@ -24,8 +24,8 @@ def fetchData():
             )
             client.switch_database('Data_Collector')
     except Exception as e:
-        print("IntrusionDetection: Error connecting to InfluxDB")
-        print("Error Message:", e)
+        print("IntrusionDetection: Error connecting to InfluxDB", flush=True)
+        print("Error Message:", e, flush=True)
 
     print("Python Function Counter: ", str(counter), flush=True)
 
@@ -34,11 +34,13 @@ def fetchData():
         query = f"SELECT * FROM ue WHERE report_num >= {(counter - 1) * 10 + 1} and report_num <= {counter * 10}"
         result = client.query(query)
 
-        print("Results:", list(result.get_points()))
+        for point in result.get_points:
+            print(f"Time: {point['time']}, Report Number: {point['report_num']}, UE: {point['ue']}", flush=True)
+        # print("Results:", list(result.get_points()), flush=True)
 
     except Exception as e:
-        print("Intrusion Detection: Error occured when trying to obtain metrics")
-        print("Error Message:", e)
+        print("Intrusion Detection: Error occured when trying to obtain metrics", flush=True)
+        print("Error Message:", e, flush=True)
 
     counter += 1
     return counter
