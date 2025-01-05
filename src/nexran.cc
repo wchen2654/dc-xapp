0d79512e1b3dcf981b00d3a6f31faa2b339fab9d
Wen Chen <wchen2654@gmail.com>
2025-01-05 17:14:56 -0600

    Code Changes

--- src/nexran.cc
+++ src/nexran.cc
@@ -220,67 +220,66 @@ bool App::secure_slicing(int rnti)
 
 	mdclog_write(MDCLOG_DEBUG,"UNBINDING SUCCESS");
 
-	// BINDING UE TO SECURE SLICE //
-	mdclog_write(MDCLOG_DEBUG,"BINDING START");		// Bind Malicious UE to Secure Slice
-	mutex.unlock();
-	bind_ue_slice(crnti_to_imsi[std::to_string(rnti)],slice2,&ae);
-	mutex.lock();
+	// // BINDING UE TO SECURE SLICE //
+	// mdclog_write(MDCLOG_DEBUG,"BINDING START");		// Bind Malicious UE to Secure Slice
+	// mutex.unlock();
+	// bind_ue_slice(crnti_to_imsi[std::to_string(rnti)],slice2,&ae);
+	// mutex.lock();
 
-	// std::memset(url, 0, sizeof(url));p
-	sprintf(url, "http://127.0.0.1:8000/v1/slices/secure_slice/ues/%s", crnti_to_imsi[std::to_string(rnti)].c_str());
+	// // std::memset(url, 0, sizeof(url));p
+	// sprintf(url, "http://127.0.0.1:8000/v1/slices/secure_slice/ues/%s", crnti_to_imsi[std::to_string(rnti)].c_str());
 
-	curl_easy_setopt(curl, CURLOPT_URL, url);
-	curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "");
-	curl_easy_setopt(curl, CURLOPT_HEADER, 1L);
+	// curl_easy_setopt(curl, CURLOPT_URL, url);
+	// curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+	// curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "");
+	// curl_easy_setopt(curl, CURLOPT_HEADER, 1L);
 	
-	ret = curl_easy_perform(curl);	
+	// ret = curl_easy_perform(curl);	
 	
-	if(ret != CURLE_OK) {
-		std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(ret) << std::endl;
-	} else {
-		// Print the response body
-		std::cout << "Response body:\n" << readBuffer << std::endl;
-	}
+	// if(ret != CURLE_OK) {
+	// 	std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(ret) << std::endl;
+	// } else {
+	// 	// Print the response body
+	// 	std::cout << "Response body:\n" << readBuffer << std::endl;
+	// }
 
-	sprintf(url, "http://127.0.0.1:8000/v1/ues/%s", crnti_to_imsi[std::to_string(rnti)].c_str());
+	// curl_easy_cleanup(curl);
+	// curl_global_cleanup();
+	// mdclog_write(MDCLOG_DEBUG,"BINDING SUCCESS");
 
-	curl_easy_cleanup(curl);
-	curl_global_cleanup();
-	mdclog_write(MDCLOG_DEBUG,"BINDING SUCCESS");
+	// sprintf(url, "http://127.0.0.1:8000/v1/ues/%s", crnti_to_imsi[std::to_string(rnti)].c_str());
 
+	// mdclog_write(MDCLOG_INFO, "Deleting url: %s", url);
+	// curl_global_init(CURL_GLOBAL_DEFAULT);
 
-	mdclog_write(MDCLOG_INFO, "Deleting url: %s", url);
-	curl_global_init(CURL_GLOBAL_DEFAULT);
+	// curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+	// curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+	// curl_easy_setopt(curl, CURLOPT_URL, url);
+	// curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L); 
 
-	curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-	curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
-	curl_easy_setopt(curl, CURLOPT_URL, url);
-	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L); 
+	// if(ret != CURLE_OK) {
+	// 	std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(ret) << std::endl;
+	// } else {
+	// 	// Print the response body
+	// 	std::cout << "Response body:\n" << readBuffer << std::endl;
+	// }
 
-	if(ret != CURLE_OK) {
-		std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(ret) << std::endl;
-	} else {
-		// Print the response body
-		std::cout << "Response body:\n" << readBuffer << std::endl;
-	}
+	// mdclog_write(MDCLOG_DEBUG, "Deleted Ue");
 
-	mdclog_write(MDCLOG_DEBUG, "Deleted Ue");
-
-	curl_easy_setopt(curl, CURLOPT_URL, "http://127.0.0.1:8000/v1/slices/secure_slice");
-	ret = curl_easy_perform(curl);	
+	// curl_easy_setopt(curl, CURLOPT_URL, "http://127.0.0.1:8000/v1/slices/secure_slice");
+	// ret = curl_easy_perform(curl);	
 
 	
-	if(ret != CURLE_OK) {
-		std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(ret) << std::endl;
-	} else {
-		// Print the response body
-		std::cout << "Response body:\n" << readBuffer << std::endl;
-	}
+	// if(ret != CURLE_OK) {
+	// 	std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(ret) << std::endl;
+	// } else {
+	// 	// Print the response body
+	// 	std::cout << "Response body:\n" << readBuffer << std::endl;
+	// }
 
-	curl_easy_cleanup(curl);
-	curl_global_cleanup();
-	mdclog_write(MDCLOG_DEBUG, "Deleted Secure Slice");
+	// curl_easy_cleanup(curl);
+	// curl_global_cleanup();
+	// mdclog_write(MDCLOG_DEBUG, "Deleted Secure Slice");
 
 }
 
