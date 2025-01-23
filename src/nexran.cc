d313f693bbadf618e4dd3f373f924ff644c6cae9
Wen Xiang <wchen2654@gmail.com>
2025-01-22 21:26:47 -0600

    Code Changes

--- src/nexran.cc
+++ src/nexran.cc
@@ -215,7 +215,7 @@ bool App::intrusion_detection()
 	{
 		if (pModule != nullptr) {
 			// Get the function from the module
-			PyObject *pFunc = PyObject_GetAttrString(pModule, "fetchData");
+			PyObject *pFunc = PyObject_GetAttrString(pModule, "main");
 
 			// Check if the function is callable
 			if (pFunc && PyCallable_Check(pFunc)) {
