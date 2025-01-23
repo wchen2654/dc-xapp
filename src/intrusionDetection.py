59f2f5884bbad7bd749df66498f83aba52cf4b08
Wen Xiang <wchen2654@gmail.com>
2025-01-23 14:22:19 -0600

    Code Changes

--- src/intrusionDetection.py
+++ src/intrusionDetection.py
@@ -270,8 +270,8 @@ def run_autoencoder_influxdb(client, reportCounter): # Training
 
             print(f"    Running optimizer.zero_grad()", flush=True)
             optimizer.zero_grad()
-            # print(f"    Passing batch through the model", flush=True)
-            # reconstructed = model(batch_data)
+            print(f"    Passing batch through the model", flush=True)
+            reconstructed = model(batch_data)
             # print(f"  Output from model: {reconstructed[0][:5]} (first sequence, first few values)", flush=True)  # Check example output
             # print(f"    Model output shape: {reconstructed.shape}", flush=True)
             # print(f"    Calculating loss", flush=True)
